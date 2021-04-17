95356bf1fe6d5ce0911eb526d56971b5a9b6d3d4
Aurelijus Banelis <aurelijus@banelis.lt>
2021-04-17 14:40:03 +0300

    Caching passwords in a file
    So update of template would not replace old users
    (easier to test the teaching material).
    
    Using best-effort password generator.
    Code should not be used for critical systems!

--- teacher/.serverless_plugins/workshopUsers.js
+++ teacher/.serverless_plugins/workshopUsers.js
@@ -19,6 +19,48 @@ class ServerlessPlugin {
     this.serverless.cli.log(message, PluginName, { color: 'orange' });
   }
 
+  async randomString(crypto, length, from, to) {
+    let promises = [];
+    for (let i = 0; i < length; i++) {
+      promises.push(crypto.randomInt(from, to)); // A-Z
+    }
+    let chars = await Promise.all(promises);
+    return String.fromCharCode(...chars);
+  }
+
+  // Not to be used for critical systems!!!
+  // Good enough random to meet AWS login requirements
+  async randomPassword(crypto) {
+    let specialCars = await this.randomString(crypto, 5, 43, 46); // A-Z
+    let uppercase = await this.randomString(crypto, 20, 69, 90); // A-Z
+    let longString = await crypto.randomUUID();
+    return longString + specialCars + uppercase;
+  }
+
+  async passwords(count) {
+    const passwordCache = "config/students.passwords.cache.json"
+    if (this.serverless.utils.fileExistsSync(passwordCache)) {
+      this.log(`Reusing old passwords ${passwordCache}...`);
+      let passwords = await this.serverless.utils.readFile(passwordCache);
+      if (count === passwords.length) {
+        return passwords
+      }
+      this.warn(`Passwords count in cache differ. Invalidating cache`);
+    }
+
+    this.log(`Generating passwords...`);
+    const crypto = await import('crypto');
+    let passwordPromises = [];
+    for (let i = 0; i < count; i++) {
+      passwordPromises.push(this.randomPassword(crypto));
+    }
+    let passwords = await Promise.all(passwordPromises);
+    this.log(`Storing passwords to cache ${passwordCache}...`);
+    await this.serverless.utils.writeFile(passwordCache, JSON.stringify(passwords, null, 2))
+    return passwords
+  }
+
+  // Main hook logic
   async beforePackage() {
     // Read students list
     let studentsFile = "config/students.local.yml"
@@ -26,8 +68,11 @@ class ServerlessPlugin {
       studentsFile = "config/students.yml"
       this.warn(`Please use "config/students.local.yml" instead of "${studentsFile}" to have local list`);
     }
-    this.log(`Reading ${studentsFile}...`, "testas");
+    this.log(`Reading ${studentsFile}...`);
     let students = await this.serverless.yamlParser.parse(studentsFile)
+
+    // Generating passwords
+    let passwords = await this.passwords(students.length);
 
     // Replacing template content
     let newResources = {};
@@ -48,6 +93,7 @@ class ServerlessPlugin {
       for (const [i, userName] of Object.entries(students)) {
         let newResource = JSON.parse(JSON.stringify(resource));
         newResource.Properties.UserName = userName;
+        newResource.Properties.LoginProfile.Password = passwords[i];
         let index = Number(i) + 1;
         let resourceName = `${name}${index}`;
         newResource.Metadata["Serverless::Plugin::WorkshopUsers"] = `generated from ${name} and ${studentsFile}`
