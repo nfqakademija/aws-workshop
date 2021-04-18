ab83c72788d3e5ecd2638e0e03ba4fb42a9c03fe
Aurelijus Banelis <aurelijus@banelis.lt>
2021-04-18 18:52:32 +0300

    UX: Easier way to get AWS credentials
    A bit cleaned up version

--- student/js/main.js
+++ student/js/main.js
@@ -17,7 +17,8 @@ var awsLogin = new Vue({
                 maxScore: 20
             },
             personalScore: [],
-            students: Config.students
+            students: Config.students,
+            showPasswords: false
         };
     },
     methods: {
@@ -58,66 +59,5 @@ var awsLogin = new Vue({
                 return false;
             }
         },
-        debugCloudTrail: async function () {
-            // Configure AWS SDK
-            this.configureAws();
-
-            // Trying to fetch cloud trail data
-            try {
-                this.loading = true;
-                let cl = new AWS.CloudTrail();
-                let startTime = Math.floor(new Date().getTime() / 1000) - (60 * 30);
-                let result = await cl.lookupEvents({
-                    StartTime: startTime,
-                    LookupAttributes: [
-                        {
-                            AttributeKey: 'Username',
-                            AttributeValue: this.aws.userName,
-                        }
-                    ]
-                }).promise();
-                this.statusMessage = JSON.stringify(result, null, 2);
-            } catch (error) {
-                this.statusMessage = 'ERROR: ' + error;
-            }
-            this.loading = false;
-        },
-        debugScore: async function () {
-            // Configure AWS SDK
-            this.configureAws();
-
-            // Trying to fetch cloud trail data
-            try {
-                this.loading = true;
-                let result = await getScore(this.aws.userName, scoreTemplate);
-                this.statusMessage = JSON.stringify(result, null, 2);
-                this.personalScore = minifiedScore(result);
-            } catch (error) {
-                this.statusMessage = 'ERROR: ' + error;
-            }
-            this.loading = false;
-        },
-        debugLambda: async function () {
-            // Configure AWS SDK
-            this.configureAws();
-
-            // Trying to fetch cloud trail data
-            try {
-                this.loading = true;
-                var lambda = new AWS.Lambda();
-                let result = await lambda.invoke({
-                    FunctionName: Config.lambda.checkTask,
-                    Payload: JSON.stringify({
-                        "some": "data"
-                    }),
-                    ClientContext: btoa(JSON.stringify({"my": {"caller": "context"}}))
-                }).promise();
-                this.statusMessage = "Status: " + result.StatusCode + "\n" + JSON.stringify(JSON.parse(result.Payload), null, 2);
-            } catch (error) {
-                this.statusMessage = 'ERROR: ' + error;
-            }
-            this.loading = false;
-
-        }
     }
 });
